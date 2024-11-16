7c1c64aca9e4f44b5951bed7392c9290a561cf70
jianzhnie <jianzhnie@126.com>
2024-11-16 17:00:11 +0800

    Update __init__.py

--- llmtoolkit/data/__init__.py
+++ llmtoolkit/data/__init__.py
@@ -10,4 +10,4 @@ Description:
 from llmtoolkit.data.tokenizers import BertTokenizer, JiebaTokenizer, Tokenizer
 from llmtoolkit.data.vocab import Vocab
 
-__all__ = ["JiebaTokenizer", "Vocab", "Tokenizer", "BertTokenizer"]
+__all__ = ['JiebaTokenizer', 'Vocab', 'Tokenizer', 'BertTokenizer']
