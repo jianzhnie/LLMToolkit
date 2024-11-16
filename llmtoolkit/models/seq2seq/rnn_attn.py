e6ffd12a93accdb94db6140353c30644ce33f49c
jianzhnie <jianzhnie@126.com>
2024-11-16 17:02:30 +0800

    Update rnn_attn.py

--- llmtoolkit/models/seq2seq/rnn_attn.py
+++ llmtoolkit/models/seq2seq/rnn_attn.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 from torch import Tensor
 
-sys.path.append("../../../")
+sys.path.append('../../../')
 from llmtoolkit.llms.vanilla.attention import AdditiveAttention
 from llmtoolkit.models.seq2seq.rnn_mt import RNNEncoder
 
@@ -64,9 +64,8 @@ class RNNAttentionDecoder(nn.Module):
         )
 
         # Initialize the Bahdanau attention module
-        self.attention = AdditiveAttention(
-            hidden_size, hidden_size, hidden_size, dropout
-        )
+        self.attention = AdditiveAttention(hidden_size, hidden_size,
+                                           hidden_size, dropout)
 
         self.dropout = nn.Dropout(dropout)
         self.fc_out = nn.Linear(hidden_size, vocab_size)
@@ -112,9 +111,8 @@ class RNNAttentionDecoder(nn.Module):
             query = torch.unsqueeze(encoder_hidden_states[-1], dim=1)
             # query shape: [batch_size, 1, hidden_size]
 
-            context = self.attention(
-                query, encoder_outputs, encoder_outputs, encoder_valid_lens
-            )
+            context = self.attention(query, encoder_outputs, encoder_outputs,
+                                     encoder_valid_lens)
             # context shape: [batch_size, 1, hidden_size]
 
             input = torch.unsqueeze(input, dim=1)
@@ -125,8 +123,7 @@ class RNNAttentionDecoder(nn.Module):
             # concatenated_features shape: [batch_size, 1, embed_size + hidden_size]
 
             decoder_outputs, decoder_hidden_state = self.rnn(
-                concatenated_features, encoder_hidden_states
-            )
+                concatenated_features, encoder_hidden_states)
             # decoder_outputs shape: [1, batch_size, hidden_size]
             # decoder_hidden_state shape: [num_layers, batch_size, hidden_size]
 
@@ -171,12 +168,10 @@ class RNNeq2SeqModel(nn.Module):
         self.num_layers = num_layers
 
         # Initialize the encoder and decoder
-        self.encoder = RNNEncoder(
-            src_vocab_size, embed_size, hidden_size, num_layers, dropout
-        )
-        self.decoder = RNNAttentionDecoder(
-            trg_vocab_size, embed_size, hidden_size, num_layers, dropout
-        )
+        self.encoder = RNNEncoder(src_vocab_size, embed_size, hidden_size,
+                                  num_layers, dropout)
+        self.decoder = RNNAttentionDecoder(trg_vocab_size, embed_size,
+                                           hidden_size, num_layers, dropout)
 
     def forward(self, src: torch.Tensor, tgt: torch.Tensor) -> torch.Tensor:
         """Forward pass of the Seq2Seq model.
@@ -202,7 +197,7 @@ class RNNeq2SeqModel(nn.Module):
         return dec_outputs
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     src_vocab_size = 10
     tgt_vocab_size = 20
     embed_size = 8
@@ -211,28 +206,27 @@ if __name__ == "__main__":
     batch_size = 4
     max_seq_len = 5
     encoder = RNNEncoder(src_vocab_size, embed_size, hiddens_size, num_layers)
-    input = torch.LongTensor(
-        [[1, 2, 4, 5, 3], [4, 3, 2, 9, 2], [1, 2, 3, 4, 4], [4, 3, 2, 1, 6]]
-    )
+    input = torch.LongTensor([[1, 2, 4, 5, 3], [4, 3, 2, 9, 2],
+                              [1, 2, 3, 4, 4], [4, 3, 2, 1, 6]])
 
-    target = torch.LongTensor(
-        [[1, 3, 4, 5, 3], [4, 3, 2, 9, 2], [1, 2, 3, 4, 4], [4, 3, 2, 1, 6]]
-    )
+    target = torch.LongTensor([[1, 3, 4, 5, 3], [4, 3, 2, 9, 2],
+                               [1, 2, 3, 4, 4], [4, 3, 2, 1, 6]])
     # input: [batch_size, max_seq_len]
     enc_outputs, enc_state = encoder(input)
     # enc_outputs: [max_seq_len, batch_size, hiddens_size]
     # enc_state: [num_layers, batch_size, hiddens_size]
     print(enc_outputs.shape, enc_state.shape)
 
-    decoder = RNNAttentionDecoder(tgt_vocab_size, embed_size, hiddens_size, num_layers)
+    decoder = RNNAttentionDecoder(tgt_vocab_size, embed_size, hiddens_size,
+                                  num_layers)
     enc_outputs = enc_outputs.permute(1, 0, 2)
-    dec_outputs, decoder_hidden_state = decoder(target, enc_outputs, enc_state, None)
+    dec_outputs, decoder_hidden_state = decoder(target, enc_outputs, enc_state,
+                                                None)
     print(dec_outputs.shape, decoder_hidden_state.shape)
 
-    print("seq2seq")
-    seq2seq = RNNeq2SeqModel(
-        src_vocab_size, tgt_vocab_size, embed_size, hiddens_size, num_layers
-    )
+    print('seq2seq')
+    seq2seq = RNNeq2SeqModel(src_vocab_size, tgt_vocab_size, embed_size,
+                             hiddens_size, num_layers)
 
     output = seq2seq(input, target)
     print(output.shape)
