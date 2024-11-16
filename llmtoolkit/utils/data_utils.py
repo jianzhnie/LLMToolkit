1ea8cda9bfd680935a2e243005e089a367c75ee7
jianzhnie <jianzhnie@126.com>
2024-11-16 17:02:55 +0800

    Update data_utils.py

--- llmtoolkit/utils/data_utils.py
+++ llmtoolkit/utils/data_utils.py
@@ -17,10 +17,10 @@ from torch.utils.data import DataLoader
 from llmtoolkit.data.vocab import Vocab
 
 # Constants
-BOS_TOKEN = "<bos>"
-EOS_TOKEN = "<eos>"
-PAD_TOKEN = "<pad>"
-UNK_TOKEN = "<unk>"
+BOS_TOKEN = '<bos>'
+EOS_TOKEN = '<eos>'
+PAD_TOKEN = '<pad>'
+UNK_TOKEN = '<unk>'
 
 
 def minEditDistance(source: str, target: str) -> int:
@@ -38,7 +38,8 @@ def minEditDistance(source: str, target: str) -> int:
     m = len(target)
 
     # 初始化编辑距离矩阵
-    dist_matrix: List[List[int]] = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+    dist_matrix: List[List[int]] = [[0 for _ in range(m + 1)]
+                                    for _ in range(n + 1)]
     for i in range(1, n + 1):
         dist_matrix[i][0] = i
     for j in range(1, m + 1):
@@ -57,9 +58,9 @@ def minEditDistance(source: str, target: str) -> int:
     return dist_matrix[n][m]
 
 
-def truncate_pad(
-    inputs: List[int], max_seq_len: int, padding_token_id: int = 0
-) -> List[int]:
+def truncate_pad(inputs: List[int],
+                 max_seq_len: int,
+                 padding_token_id: int = 0) -> List[int]:
     """Truncate and pad sequence to max sequence length."""
     if len(inputs) > max_seq_len:
         inputs = inputs[:max_seq_len]
@@ -68,45 +69,44 @@ def truncate_pad(
     return inputs
 
 
-def load_ptb_data(data_dir, split="train"):
+def load_ptb_data(data_dir, split='train'):
     """Penn Tree Bank（PTB）。该语料库取自“华尔街日报”的文章，分为训练集、验证集和测试集。
     在原始格式中，文本文件的每一行表示由空格分隔的一句话, 函数 `read_ptb_data` 将PTB数据集加载到文本行的列表中
     data_url: http://d2l-data.s3-accelerate.amazonaws.com/ptb.zip
     """
     # Readthetrainingset.
-    file_path = os.path.join(data_dir, "ptb.{}.txt".format(split))
+    file_path = os.path.join(data_dir, 'ptb.{}.txt'.format(split))
 
     with open(file_path) as f:
         raw_text = f.read()
-    return [line.split() for line in raw_text.split("\n")]
+    return [line.split() for line in raw_text.split('\n')]
 
 
 def remove_empty_paired_punc(in_str):
-    return (
-        in_str.replace("（）", "")
-        .replace("《》", "")
-        .replace("【】", "")
-        .replace("[]", "")
-    )
+    return (in_str.replace('（）', '').replace('《》',
+                                             '').replace('【】',
+                                                         '').replace('[]', ''))
 
 
 def remove_html_tags(in_str):
-    html_pattern = re.compile(r"<[^>]+>", re.S)
-    return html_pattern.sub("", in_str)
+    html_pattern = re.compile(r'<[^>]+>', re.S)
+    return html_pattern.sub('', in_str)
 
 
 def remove_control_chars(in_str):
-    control_chars = "".join(map(chr, list(range(0, 32)) + list(range(127, 160))))
-    control_chars = re.compile("[%s]" % re.escape(control_chars))
-    return control_chars.sub("", in_str)
+    control_chars = ''.join(
+        map(chr,
+            list(range(0, 32)) + list(range(127, 160))))
+    control_chars = re.compile('[%s]' % re.escape(control_chars))
+    return control_chars.sub('', in_str)
 
 
 def cleaning_wikidata(wiki_file):
     new_text = []
-    with open(wiki_file, "r", encoding="utf-8") as f_in:
+    with open(wiki_file, 'r', encoding='utf-8') as f_in:
         for line in f_in.readlines():
             line = line.strip()
-            if re.search(r"^(<doc id)|(</doc>)", line):
+            if re.search(r'^(<doc id)|(</doc>)', line):
                 print(line)
                 continue
             line = remove_empty_paired_punc(line)
@@ -122,8 +122,7 @@ def length_to_mask(lengths):
     # padding: True
     # seqs: False
     mask = torch.arange(max_len, device=lengths.device).expand(
-        lengths.shape[0], max_len
-    ) > lengths.unsqueeze(1)
+        lengths.shape[0], max_len) > lengths.unsqueeze(1)
     mask = mask.type(torch.bool)
     return mask
 
@@ -132,16 +131,17 @@ def save_pretrained_vector(vocab: Vocab, embeds: torch.tensor, save_path: str):
     """Save pretrained token vectors in a unified format, where the first line
     specifies the `number_of_tokens` and `embedding_dim` followed with all
     token vectors, one token per line."""
-    with open(save_path, "w") as writer:
-        writer.write(f"{embeds.shape[0]} {embeds.shape[1]}\n")
+    with open(save_path, 'w') as writer:
+        writer.write(f'{embeds.shape[0]} {embeds.shape[1]}\n')
         for idx, token in vocab.idx_to_token.items():
-            vec = " ".join(["{:.4f}".format(x) for x in embeds[idx]])
-            writer.write(f"{token} {vec}\n")
-    print(f"Pretrained embeddings saved to: {save_path}")
+            vec = ' '.join(['{:.4f}'.format(x) for x in embeds[idx]])
+            writer.write(f'{token} {vec}\n')
+    print(f'Pretrained embeddings saved to: {save_path}')
 
 
 def get_loader(dataset, batch_size, shuffle=True):
-    data_loader = DataLoader(
-        dataset, batch_size=batch_size, collate_fn=dataset.collate_fn, shuffle=shuffle
-    )
+    data_loader = DataLoader(dataset,
+                             batch_size=batch_size,
+                             collate_fn=dataset.collate_fn,
+                             shuffle=shuffle)
     return data_loader
