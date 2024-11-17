43d6fb7d5c28a30bff2eec582afffbfa9a6453a7
jianzhnie <jianzhnie@126.com>
2024-11-17 16:45:06 +0800

    Update __init__.py

--- llmtoolkit/llms/vanilla/attention/__init__.py
+++ llmtoolkit/llms/vanilla/attention/__init__.py
@@ -1,12 +1,3 @@
-'''
-Author: jianzhnie
-Date: 2021-12-06 15:23:18
-LastEditTime: 2021-12-06 15:23:18
-LastEditors: jianzhnie
-Description:
-
-'''
-
 from .attn_function import AdditiveAttention, DotProductAttention
 
 __all__ = ['AdditiveAttention', 'DotProductAttention']
