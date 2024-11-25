8c273c3ceb35cbb742bf5864c4863830ab201df5
jianzhnie <jianzhnie@126.com>
2024-11-25 11:08:09 +0800

    Update __init__.py

--- llmtoolkit/data/__init__.py
+++ llmtoolkit/data/__init__.py
@@ -1,12 +1,3 @@
-"""
-Author: jianzhnie
-Date: 2022-03-04 17:13:55
-LastEditTime: 2022-03-04 17:13:55
-LastEditors: jianzhnie
-Description:
-
-"""
-
 from llmtoolkit.data.tokenizers import BertTokenizer, JiebaTokenizer, Tokenizer
 from llmtoolkit.data.vocab import Vocab
 
